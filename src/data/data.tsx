aa27e5dc9c5886e6f51ae6ee85077ae019fec604
AKT311209 <khoi@tran.com.vn>
2025-01-26 09:36:38 +0700

    Update interests in aboutData and remove unused contact items

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -81,7 +81,7 @@ export const aboutData: About = {
     { label: 'Location', text: 'Hanoi, Vietnam', Icon: MapIcon },
     { label: 'DOB', text: '31-12-2009', Icon: CalendarIcon },
     { label: 'Nationality', text: 'Vietnam', Icon: FlagIcon },
-    { label: 'Interests', text: 'Running, Swimming, Coding', Icon: SparklesIcon },
+    { label: 'Interests', text: 'Programming, Running, Swimming', Icon: SparklesIcon },
     { label: 'Study', text: 'Nguyen Hue High School', Icon: AcademicCapIcon },
   ],
 };
@@ -283,26 +283,6 @@ export const contact: ContactSection = {
   headerText: 'Get in touch.',
   description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
   items: [
-    {
-      type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
-    },
-    {
-      type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
-    },
-    {
-      type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
-    },
   ],
 };
 
