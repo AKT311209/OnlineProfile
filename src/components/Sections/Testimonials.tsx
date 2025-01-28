f7962502703378e39195c815d95f9343d4ad6147
AKT311209 <khoi@tran.com.vn>
2025-01-28 10:40:45 +0700

    Add overflow handling to testimonials for improved readability

--- src/components/Sections/Testimonials.tsx
+++ src/components/Sections/Testimonials.tsx
@@ -132,7 +132,7 @@ const Testimonial: FC<{ testimonial: Testimonial; isActive: boolean }> = memo(
       ) : (
         <QuoteIcon className="h-5 w-5 shrink-0 text-white sm:h-8 sm:w-8" />
       )}
-      <div className="flex flex-col gap-y-4">
+      <div className="flex flex-col gap-y-4 overflow-y-auto">
         <p className="prose prose-sm font-medium italic text-white sm:prose-base">{text}</p>
         <p className="text-xs italic text-white sm:text-sm md:text-base lg:text-lg">-- {name}</p>
       </div>
